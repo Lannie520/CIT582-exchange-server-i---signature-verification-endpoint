9539c80d885810824cc1c0d338b54ec29e7d0504
lannie520 <33674399+Lannie520@users.noreply.github.com>
2022-03-22 09:47:01 +0800

    create

---
+++ verification_endpoint.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api
+import json
+import eth_account
+import algosdk
+
+app = Flask(__name__)
+api = Api(app)
+app.url_map.strict_slashes = False
+
+@app.route('/verify', methods=['GET','POST'])
+def verify():
+    content = request.get_json(silent=True)
+
+    #Check if signature is valid
+    payload = content['payload']
+    private_key = content['sig']
+    public_key = payload['pk']
+    platform = payload['platform']
+    msg = json.dumps(payload)
+    result = False
+    if platform == 'Ethereum':
+        msg_eth = eth_account.messages.encode_defunct(txt=msg)
+        if eth_account.Account.recover_message(msg_eth, signature=private_key) == public_key:
+            print("Eth sig verifies!")
+            result = True
+    elif platform == 'Algorand':
+        if algosdk.util.verify_bytes(message.encode('utf-8'),private_key,public_key):
+            print("Algo sig verifies!")
+            result = True #Should only be true if signature validates
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(port='5002')
